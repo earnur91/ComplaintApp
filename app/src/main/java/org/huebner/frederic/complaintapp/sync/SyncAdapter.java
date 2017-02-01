465ecb2c1b88203ebbfb3ee6cfdcf1b8bac04a02
Frederic Hübner <frederic.huebner@gmail.com>
2017-02-01 16:46:59 +0100

    Create SyncAdapter class

---
+++ app/src/main/java/org/huebner/frederic/complaintapp/sync/SyncAdapter.java
@@ -0,0 +1,20 @@
+package org.huebner.frederic.complaintapp.sync;
+
+import android.accounts.Account;
+import android.content.AbstractThreadedSyncAdapter;
+import android.content.ContentProviderClient;
+import android.content.Context;
+import android.content.SyncResult;
+import android.os.Bundle;
+
+public class SyncAdapter extends AbstractThreadedSyncAdapter {
+
+    public SyncAdapter (Context context, Boolean autoInitialize) {
+        super(context, autoInitialize);
+    }
+
+    @Override
+    public void onPerformSync(Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) {
+
+    }
+}
