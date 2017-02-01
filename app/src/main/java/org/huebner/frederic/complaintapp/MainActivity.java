af4449758fad97dad60a817efb17efab0fd494f4
Frederic Hübner <frederic.huebner@gmail.com>
2017-02-01 14:58:47 +0100

    Use ListActivity for MainActivity

--- app/src/main/java/org/huebner/frederic/complaintapp/MainActivity.java
+++ app/src/main/java/org/huebner/frederic/complaintapp/MainActivity.java
@@ -1,13 +1,30 @@
 package org.huebner.frederic.complaintapp;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
     }
 }
