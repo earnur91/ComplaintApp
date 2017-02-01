e82b85781a88bab8b92048a9fda3a5bff381535a
Frederic Hübner <frederic.huebner@gmail.com>
2017-02-01 14:45:19 +0100

    Add SyncState enum

---
+++ app/src/main/java/org/huebner/frederic/complaintapp/content/SyncState.java
@@ -0,0 +1,8 @@
+package org.huebner.frederic.complaintapp.content;
+
+/**
+ * Represents the synchronisation state of a specific entity.
+ */
+public enum SyncState {
+    NOOP, CREATE, UPDATE, DELETE, CONFLICTED;
+}
