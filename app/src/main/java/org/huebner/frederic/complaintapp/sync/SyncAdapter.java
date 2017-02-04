05420bfa64e643a7a1ad5cf0166ab9aa29826bb6
Frederic Hübner <frederic.huebner@gmail.com>
2017-02-04 11:38:36 +0100

    Create method for creation sync

--- app/src/main/java/org/huebner/frederic/complaintapp/sync/SyncAdapter.java
+++ app/src/main/java/org/huebner/frederic/complaintapp/sync/SyncAdapter.java
@@ -6,15 +6,27 @@ import android.content.ContentProviderClient;
 import android.content.Context;
 import android.content.SyncResult;
 import android.os.Bundle;
+import android.util.Log;
 
 public class SyncAdapter extends AbstractThreadedSyncAdapter {
 
-    public SyncAdapter (Context context, Boolean autoInitialize) {
+    private static final String TAG = "SyncAdpater";
+
+    public SyncAdapter(Context context, Boolean autoInitialize) {
         super(context, autoInitialize);
     }
 
     @Override
     public void onPerformSync(Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) {
         // TODO: Implement sync strategy here
+        Log.d(TAG, "Synchronization started");
+
+        syncCreatedEntities(provider, syncResult);
+
+        Log.d(TAG, "Synchronization finished");
+    }
+
+    private void syncCreatedEntities(ContentProviderClient contentProvider, SyncResult syncResult) {
+
     }
 }
