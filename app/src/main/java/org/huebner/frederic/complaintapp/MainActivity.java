239f04f07d64f3c7ff6490d3538e12cf245bbf9b
Frederic Hübner <frederic.huebner@gmail.com>
2017-02-05 18:11:15 +0100

    Update user interface

--- app/src/main/java/org/huebner/frederic/complaintapp/MainActivity.java
+++ app/src/main/java/org/huebner/frederic/complaintapp/MainActivity.java
@@ -1,20 +1,30 @@
 package org.huebner.frederic.complaintapp;
 
-import android.app.ListActivity;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
-public class MainActivity extends ListActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.complaint_toolbar);
+        setSupportActionBar(toolbar);
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -26,4 +36,5 @@ public class MainActivity extends ListActivity {
     protected void onPause() {
         super.onPause();
     }
+
 }
