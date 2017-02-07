4a688b53b544b83ed939325a2d09fb219f629f40
Frederic Hübner <frederic.huebner@gmail.com>
2017-02-07 12:17:33 +0100

    Add create complaint logic

--- app/src/main/java/org/huebner/frederic/complaintapp/CreateComplaintActivity.java
+++ app/src/main/java/org/huebner/frederic/complaintapp/CreateComplaintActivity.java
@@ -1,11 +1,25 @@
 package org.huebner.frederic.complaintapp;
 
+import android.content.ContentValues;
+import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.huebner.frederic.complaintapp.content.Complaint;
+import org.huebner.frederic.complaintapp.content.ComplaintContentProvider;
+import org.huebner.frederic.complaintapp.content.ProcessingStatus;
+import org.huebner.frederic.complaintapp.content.SyncState;
 
 public class CreateComplaintActivity extends AppCompatActivity {
+
+    private EditText nameEdit;
+    private EditText locationEdit;
+    private EditText complaintTextEdit;
+    private Button createButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +29,37 @@ public class CreateComplaintActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
+
+
+        nameEdit = (EditText) findViewById(R.id.input_name);
+        locationEdit = (EditText) findViewById(R.id.input_location);
+        complaintTextEdit = (EditText) findViewById(R.id.input_complaintText);
+        createButton = (Button) findViewById(R.id.btn_create);
+
+    }
+
+    public void createComplaint(View view) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(Complaint.NAME, nameEdit.getText().toString());
+        contentValues.put(Complaint.LOCATION, locationEdit.getText().toString());
+        contentValues.put(Complaint.COMPLAINT_TEXT, complaintTextEdit.getText().toString());
+        contentValues.put(Complaint.PROCESSING_STATUS, ProcessingStatus.CREATED.name());
+        contentValues.put(Complaint.SYNC_STATE, SyncState.CREATE.name());
+        getContentResolver().insert(ComplaintContentProvider.CONTENT_URI, contentValues);
+
+        requestUpload();
+        finish();
+    }
+
+    private void requestUpload() {
+        requestSync(true);
+    }
+
+    private void requestSync(boolean upload) {
+//        Bundle extras = new Bundle();
+//        extras.putBoolean(ContentResolver.SYNC_EXTRAS_UPLOAD, upload);
+//        extras.putBoolean(ContentResolver.SYNC_EXTRAS_EXPEDITED, true);
+//
+//        ContentResolver.requestSync(, ComplaintContentProvider.AUTHORITY, extras);
     }
 }
